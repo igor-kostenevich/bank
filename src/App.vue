23448305dd74f774479b8b157859adf4c642df2b
i-kostenevich <igor.kostenevich557@gmail.com>
2021-02-10 02:26:15 +0200

    added layouts & pages

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <component :is="layout + '-layout'" v-if="layout"></component>
+  <router-view />
 </template>
 
+<script>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import MainLayout from './layout/MainLayout'
+import AuthLayout from './layout/AuthLayout'
+
+export default {
+  setup () {
+    const route = useRoute()
+
+    console.log(route.meta.layout)
+
+    return {
+      layout: computed(() => route.meta.layout)
+    }
+  },
+  components: { AuthLayout, MainLayout }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/layout/AuthLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <div class="card"></div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
